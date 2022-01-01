37eb8ffc4887feef87399d7ed50980f3f229530f
david vargas <kaperuzzoo@gmail.com>
2022-01-01 19:43:36 +0100

    cap2

---
+++ CASTANHA.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Capitulos</title>
+</head>
+<body>
+    <h1>O Xeito Das Froitas</h1>
+    <main>
+        <article>
+            <h3>Capitulo 1</h3>
+            <h2>Castaña</h2>
+            <div>
+                <ul>
+                    <li>Iris queria llamarse Castaña</li>
+                    <li>No le gusta el nombre Iris</li>
+                    <li>Dice que las castañas son riquisimas</li>
+                    <li>Envez de contar obejas contaba castañas para dormir</li>
+                    <li>Y dode la abuela tenia un castañero</li>
+                    <li>Le dijo al padre que era malo por no aberselo consultado a ella de pequeña</li>
+                    <li>Y el padre le dijo que no podia consultarselo porque era pequeña y no sabia hablar ni entendia</li>
+                    <li>Y la niña se fue enfadada</li>
+                    <li>A la bodega se va hay porque es el sitio donde nadie le puede hacer nada</li>
+                </ul>
+                <div>
+                    <img src="img/elxeitodasfroitas.jpg" alt="" width=500>
+                </div>
+            </div>
+        </article>
+        <article>
+            <h3>Capitulo 2</h3>
+            <h2>Arroz huevos con chorizo</h2>
+            <div>
+                <ul>
+                    <li>A la niña q al cabo de 4 dias olvido el asunto de castaña</li>
+                    <li>Al final dijo asi misma que Iris no le disgustaba</li>
+                    <li>A Iris le interesaba comer castañas y tenia un predilecto con la comida </li>
+                    <li>Y a la noche comio arroz huevos con chorizo</li>
+                    <li>La madre le acosto y no pegaba ojo</li>
+                    <li>Se levanto a decirle a los padres que le dolia la tripa</li>
+                    <li>Y dijo que no volveria a comer arroz huevos con chorizo</li>
+                </ul>
+                <div>
+                    <img src="img/yonki.png" alt="" width=400>
+                </div>
+            </div>
+        </article>
+    </main>
+
+
+
+
+
+
+
+
+
+
+
+
+
+</body>
+</html>
